43c524590f195ecfe47bd5743bc3fc4269e7ab42
Sriharini <132827243+SrihariniShivagnanam@users.noreply.github.com>
2024-08-28 15:31:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error('MongoDB connection error:',err);
+        throw err;
+    }
+}
+module.exports = connectDB;
